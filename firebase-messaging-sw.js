3213a6fc258ce1c08a79e00d8019e818d1c71ef1
courtneyzarzycki-svg <courtney.zarzycki@gmail.com>
2026-06-25 18:31:12 -0700

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+    apiKey: "AIzaSyA6hEEmGv3X0L5sQbZP2YYUQu-sixDqsPM",
+    authDomain: "family-hub-2-4a707.firebaseapp.com",
+    databaseURL: "https://family-hub-2-4a707-default-rtdb.firebaseio.com",
+    projectId: "family-hub-2-4a707",
+    storageBucket: "family-hub-2-4a707.firebasestorage.app",
+    messagingSenderId: "533379938918",
+    appId: "1:533379938918:web:949fbd193d22ff5b074ba7"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/icon.png"
+    }
+  );
+});
