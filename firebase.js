827a6239fc77ebb58a83bb5e6d69da37e593677f
courtneyzarzycki-svg <courtney.zarzycki@gmail.com>
2026-06-25 18:23:11 -0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,13 +3,14 @@ import { getMessaging } from "firebase/messaging";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_ID",
-  appId: "YOUR_APP_ID"
-};
+    apiKey: "AIzaSyA6hEEmGv3X0L5sQbZP2YYUQu-sixDqsPM",
+    authDomain: "family-hub-2-4a707.firebaseapp.com",
+    databaseURL: "https://family-hub-2-4a707-default-rtdb.firebaseio.com",
+    projectId: "family-hub-2-4a707",
+    storageBucket: "family-hub-2-4a707.firebasestorage.app",
+    messagingSenderId: "533379938918",
+    appId: "1:533379938918:web:949fbd193d22ff5b074ba7"
+  };
 
 export const app = initializeApp(firebaseConfig);
 export const messaging = getMessaging(app);
